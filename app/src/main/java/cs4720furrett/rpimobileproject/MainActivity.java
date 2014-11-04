ac2bb0f39e43a7da764fdcc2ad0953fac881af23
Craig <craigshockleyhunter@gmail.com>
2014-11-03 23:32:59 -0500

    initial derp, set up post text and button, doesn't work yet

--- app/src/main/java/cs4720furrett/rpimobileproject/MainActivity.java
+++ app/src/main/java/cs4720furrett/rpimobileproject/MainActivity.java
@@ -4,6 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 
 public class MainActivity extends Activity {
@@ -35,5 +47,41 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /* Sends post */
+    public void sendPost(View view) throws IOException, JSONException {
+
+        EditText editText  = (EditText)findViewById(R.id.editText);
+        String postURL = editText.getText().toString();
+
+        String json = "{\n" +
+                "\"lights\": [\n" +
+                "\n" +
+                "{\"lightId\": 1, \"red\":255,\"green\":0,\"blue\":0, \"intensity\": 0.3},\n" +
+                "{\"lightId\": 10, \"red\":0,\"green\":0,\"blue\":255, \"intensity\": 0.5},\n" +
+                "{\"lightId\": 15, \"red\":255,\"green\":0,\"blue\":255, \"intensity\": 0.5},\n" +
+                "{\"lightId\": 20, \"red\":0,\"green\":255,\"blue\":0, \"intensity\": 0.7}],\n" +
+                "\n" +
+                "\"propagate\": true\n" +
+                "}";
+
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+
+        //make connection to path
+        HttpPost httpPost = new HttpPost(postURL);
+
+        //json object to be sent
+        JSONObject holder = new JSONObject(json);
+
+        StringEntity se = new StringEntity(holder.toString());
+
+        httpPost.setEntity(se);
+        httpPost.setHeader("Accept", "application/json");
+        httpPost.setHeader("Content-type", "application/json");
+
+        //Handles what is returned from the page
+        ResponseHandler responseHandler = new BasicResponseHandler();
+        httpClient.execute(httpPost, responseHandler);
     }
 }
