e6cf846540777f678ab6ca098de4e35b4c6ca50a
Mitchell Smith <zephyrthenoble@gmail.com>
2014-11-13 21:47:38 -0500

    Working accelerometer feature
    Craig ruins everything

--- app/src/main/java/cs4720furrett/rpimobileproject/MainActivity.java
+++ app/src/main/java/cs4720furrett/rpimobileproject/MainActivity.java
@@ -1,12 +1,20 @@
 package cs4720furrett.rpimobileproject;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.FloatMath;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.app.AlertDialog;
 
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpPost;
@@ -20,14 +28,54 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
+
+    //The dialog that pops up
+    private AlertDialog alertDialog;
+    final Context context = this;
+
+    //Variables to use the accelerometer
+    private SensorManager sensorMan;
+    private Sensor accelerometer;
+    private float[] mGravity;
+    private float mAccel;
+    private float mAccelCurrent;
+    private float mAccelLast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //setting up the accelerometer
+        sensorMan = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorMan.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mAccel = 0.00f;
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;
+        sensorMan.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+        buildDialog();
+
+
     }
 
+    public void buildDialog() {
+        //setting up the dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage("You moved the Android device!")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                });
+
+
+        alertDialog = builder.create();
+    }
+
+    public void onClick(DialogInterface dialog, int which) {
+// here you can add functions
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -55,11 +103,17 @@ public class MainActivity extends Activity {
     public void sendPost(View view) throws IOException, JSONException {
 
         //get url from field
-        EditText editText  = (EditText)findViewById(R.id.editText);
+        EditText editText = (EditText) findViewById(R.id.editText);
         String postURL = editText.getText().toString();
         if (!postURL.startsWith("http://")) postURL = "http://" + postURL;
         //String postURL = "http://cs4720.cs.virginia.edu/rpi/?username=csh7kd"; //hard coding for now
         new SendPost().execute(postURL);
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 
     private class SendPost extends AsyncTask<String, Void, Void> {
@@ -108,6 +162,38 @@ public class MainActivity extends Activity {
                 e.printStackTrace();
             }
             return null;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sensorMan.registerListener((android.hardware.SensorEventListener) this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorMan.unregisterListener((android.hardware.SensorEventListener) this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            // Shake detection
+            mGravity = event.values.clone();
+            float x = mGravity[0];
+            float y = mGravity[1];
+            float z = mGravity[2];
+            mAccelLast = mAccelCurrent;
+            mAccelCurrent = FloatMath.sqrt(x * x + y * y + z * z);
+            float delta = mAccelCurrent - mAccelLast;
+            mAccel = mAccel * 0.9f + delta;
+            // Make this higher or lower according to how much
+            // motion you want to detect
+            if (mAccel > 3) {
+                alertDialog.show();
+            }
         }
     }
 }
