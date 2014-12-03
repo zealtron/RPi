6956db1c5fad23f561ef2aed1ddcbf336e77406f
Mitchell Smith <zephyrthenoble@gmail.com>
2014-12-02 21:15:52 -0500

    Toast when settings is shaken

--- app/src/main/java/cs4720furrett/rpimobileproject/MySettings.java
+++ app/src/main/java/cs4720furrett/rpimobileproject/MySettings.java
@@ -1,22 +1,36 @@
 package cs4720furrett.rpimobileproject;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.FloatMath;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 
-public class MySettings extends Activity {
+public class MySettings extends Activity implements SensorEventListener {
+    final Context context = this;
     private SharedPreferences pref;
     private SharedPreferences.Editor editor;
     private String url = "";
     private ToggleButton toggle;
+    private SensorManager sensorMan;
+    private Sensor accelerometer;
+    private float[] mGravity;
+    private float mAccel;
+    private float mAccelCurrent;
+    private float mAccelLast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,8 +71,58 @@ public class MySettings extends Activity {
                 System.out.println(paramInt);
             }
         });
+        setupAccelerometer();
     }
 
+    public void setupAccelerometer() {
+        //setting up the accelerometer
+        sensorMan = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorMan.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mAccel = 0.00f;
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;
+        sensorMan.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            // Shake detection
+            mGravity = event.values.clone();
+            float x = mGravity[0];
+            float y = mGravity[1];
+            float z = mGravity[2];
+            mAccelLast = mAccelCurrent;
+            mAccelCurrent = FloatMath.sqrt(x * x + y * y + z * z);
+            float delta = mAccelCurrent - mAccelLast;
+            mAccel = mAccel * 0.9f + delta;
+            // Make this higher or lower according to how much
+            // motion you want to detect
+            if (mAccel > 3) {
+                CharSequence text = "You shook the device!";
+                Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+                //alertDialog.show();
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sensorMan.registerListener((android.hardware.SensorEventListener) this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorMan.unregisterListener((android.hardware.SensorEventListener) this);
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
